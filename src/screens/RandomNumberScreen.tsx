aead4fed5e790a3e7b5816f4f174d355fc02cbc9
ncquan04 <chiquan02122004@gmail.com>
2025-03-07 23:15:56 +0700

    RandomNumber

--- src/screens/RandomNumberScreen.tsx
+++ src/screens/RandomNumberScreen.tsx
@@ -9,21 +9,33 @@ import RandomCustomModal from '../modals/RandomCustomModal';
 
 const RandomNumberStore = () => {
   const [randomNumber, setRandomNumber] = useState<string>('0');
+  const [startNumber, setStartNumber] = useState<number>(1);
+  const [endNumber, setEndNumber] = useState<number>(100);
+  const [duration, setDuration] = useState<number>(5);
+  const [isRandoming, setIsRandoming] = useState<boolean>(false);
+  const [tempNumber, setTempNumber] = useState<number>(startNumber);
   const [RandomCustomModalVisible, setRandomCustomModalVisible] = useState<boolean>(false);
   const [RandomHistoryModalVisible, setRandomHistoryModalVisible] = useState<boolean>(false);
+
+  const StartRandom = () => {
+    
+  }
   
   return (
     <View style={{ width: '100%', height: '100%', backgroundColor: 'white', flexDirection: 'column', 
     justifyContent: 'space-between', alignItems: 'center', paddingTop: '60%', paddingBottom: '20%'}}
     >
-      <Text style={{fontSize: 120, fontWeight: 600}}>{randomNumber}</Text>
+      <Text style={{fontSize: 120, fontWeight: 600}}>{isRandoming ? tempNumber : randomNumber}</Text>
       <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '70%', 
         backgroundColor: '#305b69', paddingVertical: 2, borderRadius: 100}}
       >
         <TouchableOpacity style={{width: '30%', justifyContent: 'center', alignItems: 'center'}}>
           <HistoryIcon width={40} height={40} fill={'white'}/>
         </TouchableOpacity>
-        <TouchableOpacity style={{width: '40%', justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity 
+          style={{width: '40%', justifyContent: 'center', alignItems: 'center'}}
+          onPress={StartRandom}
+        >
           <StartIcon width={80} height={80}/>
         </TouchableOpacity>
         <TouchableOpacity 
