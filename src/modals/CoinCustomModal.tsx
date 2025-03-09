75ee77ac9cdfd9fa998d7f7fceaddea33f83cee2
ncquan04 <chiquan02122004@gmail.com>
2025-03-09 09:54:01 +0700

    Flipping Coin 1

---
+++ src/modals/CoinCustomModal.tsx
@@ -0,0 +1,75 @@
+import { View, Text, TouchableOpacity, Modal, ScrollView, Image } from 'react-native'
+import React, { useContext } from 'react'
+import BackIcon from '../../assets/icons/BackIcon';
+import { LanguageContext } from '../contexts/LanguageContext';
+
+interface CoinCustomModalProps {
+    coinIndex: number;
+    setCoinIndex: (index: number) => void;
+    coinCustomModalVisible: boolean;
+    setCoinCustomModalVisible: (visible: boolean) => void;
+    heads: { [key: number]: any };
+    tails: { [key: number]: any };
+}
+
+const CoinCustomModal = ({ coinIndex, setCoinIndex, coinCustomModalVisible, setCoinCustomModalVisible, heads, tails }: CoinCustomModalProps) => {
+    const {t} = useContext(LanguageContext);
+
+    const handleChangeCoins = (index: number) => {
+        setCoinIndex(index + 1);
+        setCoinCustomModalVisible(false);
+    }
+
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={coinCustomModalVisible}
+            onRequestClose={() => {
+                setCoinCustomModalVisible(!coinCustomModalVisible);
+            }}
+        >
+            <View style={{width: '100%', height: '100%', flexDirection: 'column', backgroundColor: 'white', alignItems: 'center'}}>
+                <View 
+                    style={{width: '100%', height: '10%', paddingHorizontal: 20, flexDirection: 'row', 
+                    justifyContent: 'space-between', alignItems: 'center', position: 'relative'}}
+                >
+                    <TouchableOpacity
+                        style={{zIndex: 10}}
+                        onPress={() => setCoinCustomModalVisible(false)}
+                    >
+                        <BackIcon width={40} height={40}/>
+                    </TouchableOpacity>
+                    <Text 
+                        style={{position: 'absolute', left: 0, right: 0, textAlign: 'center', fontSize: 30, fontWeight: "500"}}
+                    >{
+                        t['Coins']}
+                    </Text>
+                </View>
+                <ScrollView style={{width: '80%', flexDirection: 'column', marginTop: 20}}>
+                    {Object.keys(heads).map(((key: string, index) => {
+                        return (
+                            <TouchableOpacity 
+                                key={index}
+                                style={{width: '100%', height: 150, flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
+                                marginTop: 30, backgroundColor: '#305b69', borderRadius: 10, paddingHorizontal: '25%'}}
+                                onPress={() => handleChangeCoins(index)}
+                            >
+                                <Image
+                                    source={heads[index + 1]}
+                                    style={{width: '80%', height: '80%', objectFit: 'contain'}}
+                                />
+                                <Image
+                                    source={tails[index + 1]}
+                                    style={{width: '80%', height: '80%', objectFit: 'contain'}}
+                                />
+                            </TouchableOpacity>
+                        )
+                    }))}
+                </ScrollView>
+            </View>
+        </Modal>
+    )
+}
+
+export default CoinCustomModal
--- src/screens/FlippingCoinScreen.tsx
+++ src/screens/FlippingCoinScreen.tsx
@@ -3,18 +3,49 @@ import React, { useState } from 'react'
 import StartIcon from '../../assets/icons/StartIcon'
 import HistoryIcon from '../../assets/icons/HistoryIcon'
 import CustomIcon from '../../assets/icons/CustomIcon'
+import RestartIcon from '../../assets/icons/RestartIcon'
+import CoinCustomModal from '../modals/CoinCustomModal'
 
-const FlippingCoinStore = () => {
+const FlippingCoinScreen = () => {
   const [coinIndex, setCoinIndex] = useState<number>(1);
+  const [coinCustomModalVisible, setCoinCustomModalVisible] = useState<boolean>(false);
+
+  const heads: { [key: number]: any } = {
+    1: require('../../assets/coins/heads_1.png'),
+    2: require('../../assets/coins/heads_2.png'),
+    3: require('../../assets/coins/heads_3.png'),
+    4: require('../../assets/coins/heads_4.png'),
+    5: require('../../assets/coins/heads_5.png')
+  }
+
+  const tails: { [key: number]: any } = {
+    1: require('../../assets/coins/tails_1.png'),
+    2: require('../../assets/coins/tails_2.png'),
+    3: require('../../assets/coins/tails_3.png'),
+    4: require('../../assets/coins/tails_4.png'),
+    5: require('../../assets/coins/tails_5.png')
+  }
 
   return (
-    <View style={{ width: '100%', height: '100%', backgroundColor: 'white', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', paddingTop: '40%', paddingBottom: '20%' }}>
-      <View style={{ width: '70%', height: '70%'}}>
-        {coinIndex === 1 && <Image source={require('../../assets/coins/heads_1.png')} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />}
-        {coinIndex === 2 && <Image source={require('../../assets/coins/heads_2.png')} />}
-        {coinIndex === 3 && <Image source={require('../../assets/coins/heads_3.png')} />}
+    <View style={{ width: '100%', height: '100%', backgroundColor: 'white', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', paddingBottom: '20%' }}>
+      <View style={{width: '70%', height: '10%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginLeft: 'auto', marginRight: 20}}>
+        <View>
+          <Image/>
+        </View>
+        <View>
+          <Image/>
+        </View>
+        <TouchableOpacity>
+          <RestartIcon width={30} height={30} />
+        </TouchableOpacity>
       </View>
-
+      
+      <View style={{ width: '70%', height: '70%'}}>
+        <Image
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+          source={heads[coinIndex]}
+        />
+      </View>
       
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '70%', backgroundColor: '#305b69', paddingVertical: 2, borderRadius: 100 }}>
       <TouchableOpacity style={{ width: '30%', justifyContent: 'center', alignItems: 'center' }} onPress={() => {}}>
@@ -23,12 +54,21 @@ const FlippingCoinStore = () => {
       <TouchableOpacity style={{ width: '40%', justifyContent: 'center', alignItems: 'center' }} onPress={() => {}}>
         <StartIcon width={80} height={80} />
       </TouchableOpacity>
-      <TouchableOpacity style={{ width: '30%', justifyContent: 'center', alignItems: 'center' }} onPress={() => {}}>
+      <TouchableOpacity style={{ width: '30%', justifyContent: 'center', alignItems: 'center' }} onPress={() => setCoinCustomModalVisible(true)}>
         <CustomIcon width={40} height={40} fill={'white'} />
       </TouchableOpacity>
       </View>
+
+      <CoinCustomModal
+        coinIndex={coinIndex}
+        setCoinIndex={setCoinIndex}
+        coinCustomModalVisible={coinCustomModalVisible}
+        setCoinCustomModalVisible={setCoinCustomModalVisible}
+        heads={heads}
+        tails={tails}
+      />
     </View>
   )
 }
 
-export default FlippingCoinStore
+export default FlippingCoinScreen
