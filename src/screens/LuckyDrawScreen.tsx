8c22062c2da0c8e4acdbd8671b7bac213e31c06a
ncquan04 <chiquan02122004@gmail.com>
2025-03-09 23:03:27 +0700

    LuckyDraw 1

--- src/screens/LuckyDrawScreen.tsx
+++ src/screens/LuckyDrawScreen.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useContext } from 'react'
+import { LanguageContext } from '../contexts/LanguageContext'
 
 const LuckyDrawStore = () => {
+  const {t} = useContext(LanguageContext);
+
   return (
-    <View>
-      <Text>LuckyDrawStore</Text>
+    <View style={{width: '100%', height: '100%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{width: '80%', height: '17%', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <Text>{t['1. Everyone (2 - 10 people) HOLD one finger on the screen']}</Text>
+        <Text>{t['2. Wait 3 seconds then remove your finger from the screen']}</Text>
+        <Text>{t['3. The winner will be highlighted on the screen']}</Text>
+      </View>
+      <View style={{width: '30%', height: '15%', marginTop: 20}}>
+        <Image
+          source={require('../../assets/images/randomHandTouch.png')}
+          style={{width: '100%', height: '100%'}}
+        />
+      </View>
     </View>
   )
 }
